60cbf5be46f5bd0b325290ce1a4f120396df802a
Cristian <cgalarzalppa@gmail.com>
2024-12-11 19:02:55 -0300

    testeando y validando la creacion de usuarios desde node a mongo

--- controllers/veterinarioCotroller.js
+++ controllers/veterinarioCotroller.js
@@ -1,9 +1,19 @@
 import { request } from "express";
+import Veterinario from "../models/Veterinario.js";
 
-const registrar = (req, res) => {
+const registrar = async (req, res) => {
+    //descontruction de req.body
+    //const {nombre, email, password} = req.body;
+    try{
+        //guardar un nuevo Veterinario
+        const veterinario = new Veterinario(req.body);
+        const veterinarioGuardado = await veterinario.save();
 
-    console.log(req.body);
-    res.json({msj: "registrando usuario.."});
+        res.json(veterinarioGuardado);
+    }catch (error){
+        console.log(error);
+    }
+
 };
 
 const perfil = (req, res) => {
