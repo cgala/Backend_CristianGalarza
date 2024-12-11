3c414111a774f9566df7b25bc9a212949d4e39c6
Cristian <cgalarzalppa@gmail.com>
2024-12-11 13:00:56 -0300

    agregando rutas

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import conectarDB from "./config/db.js";
-
+import veterinarioRoutes from "./routes/veterinarioRoutes.js"
 const app = express();
 dotenv.config();
 
 conectarDB();
 
-
-app.use('/', (req, res) => {
-    res.send('Hola Mundo')
-});
+// al hacer una peticion a "/api/veterinarios", se conecta al archivo  veterinarioRouters.js
+app.use("/api/veterinarios",veterinarioRouters );
 
 // SI NO EXITE CONECTAR AL PUERTO 4000
 const PORT = process.env.PORT || 4000
