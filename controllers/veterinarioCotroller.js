5348f1d168ccdc0801988499d16d5cbd4a189c8b
Cristian <cgalarzalppa@gmail.com>
2024-12-13 11:43:26 -0300

    Validacion para evitar usuarios duplicados

--- controllers/veterinarioCotroller.js
+++ controllers/veterinarioCotroller.js
@@ -4,6 +4,17 @@ import Veterinario from "../models/Veterinario.js";
 const registrar = async (req, res) => {
     //descontruction de req.body
     //const {nombre, email, password} = req.body;
+    const { email } = req.body
+
+    // prevenir  usuarios duplicados
+    //finOne busca por registro  en la bd
+    const existeUsuario = await Veterinario.findOne({email:email});
+
+    if (existeUsuario){
+        const error = new Error("Usuario ya registrado");
+        return res.status(400).json({msg: error.message});
+    }
+
     try{
         //guardar un nuevo Veterinario
         const veterinario = new Veterinario(req.body);
