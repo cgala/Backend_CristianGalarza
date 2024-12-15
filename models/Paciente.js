ca1bfa323bba03194dbc9793c8f2361e6651d956
Cristian <cgalarzalppa@gmail.com>
2024-12-15 18:21:20 -0300

    creando el modelo de pacientes

---
+++ models/Paciente.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const pacienteSchema = mongoose.Schema({
+    nombre:{
+        type: String,
+        require: true,
+    },
+    propietario: {
+        type: String,
+        require: true,
+    },
+    email:{
+        type: String,
+        require: true,
+    },
+    nombre:{
+        type: String,
+        require: true,
+    },
+    nombre:{
+        type: String,
+        require: true,
+    },
+    veterinario: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Veterinario' //igual al nombre del modelo al cual me quiero relacionar
+    },
+    },
+    {
+        //agrega la fecha de creacion y modificacion
+        timestamps:true,
+});
+
+const Paciente = mongoose.model("Paciente", pacienteSchema);
+
+export default Paciente;
