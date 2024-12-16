a38ece679db96558a77e0f01880259295fb12ccc
Cristian <cgalarzalppa@gmail.com>
2024-12-16 12:19:47 -0300

    se agrego funcionalidad a la funcion de eliminar paciente

--- controllers/pacienteController.js
+++ controllers/pacienteController.js
@@ -16,17 +16,81 @@ const agregarPaciente = async (req, res) => {
 
 };
 //usando la variable de sesionde express  al momento que se logea el veterinario
-const obtenerPaciente = async (req, res) => {
+const obtenerPacientes = async (req, res) => {
     const pacientes = await Paciente.find().where('veterinario').equals(req.veterinario);
 
     res.json(pacientes);
 };
 
-const obtenerPacientes = async (req, res) =>{};
+const obtenerPaciente = async (req, res) => {
+    const { id } = req.params;
+    const paciente = await Paciente.findById(id);
+    
+    if(!paciente){
+        return res.status(404).json({msj: 'No encontrado'});
+    }
 
-const actualizarPaciente = async (req, res) =>{};
+    //validamos que el id del veterinario en el objeto paciente obtenido, seal emismo que el de la sesion
+    if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()){
+        return res.json({msj: "accion no permitida"});
+    }
+    if (paciente) {
+        res.json(paciente);
+    }
 
-const eliminarPaciente = async (req, res) =>{};
+};
+
+const actualizarPaciente = async (req, res) =>{
+    const { id } = req.params;
+    const paciente = await Paciente.findById(id);
+    
+    if(!paciente){
+        return res.status(404).json({msj: 'No encontrado'});
+    }
+
+    //validamos que el id del veterinario en el objeto paciente obtenido, seal emismo que el de la sesion
+    if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()){
+        return res.json({msj: "accion no permitida"});
+    }
+
+    // actualizar paciente
+    paciente.nombre = req.body.nombre || paciente.nombre;
+    paciente.propietario = req.body.propietario || paciente.propietario;
+    paciente.email = req.body.email || paciente.email;
+    paciente.fecha = req.body.fecha || paciente.fecha;
+    paciente.sintomas = req.body.sintomas || paciente.sintomas;
+
+    try {
+        const pacienteActualizado = await paciente.save();
+        res.json(pacienteActualizado);
+
+    } catch (error){
+        console.log(error);
+    }
+
+};
+
+const eliminarPaciente = async (req, res) =>{
+    const { id } = req.params;
+    const paciente = await Paciente.findById(id);
+    
+    if(!paciente){
+        return res.status(404).json({msj: 'No encontrado'});
+    }
+
+    //validamos que el id del veterinario en el objeto paciente obtenido, seal emismo que el de la sesion
+    if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()){
+        return res.json({msj: "accion no permitida"});
+    }
+
+    try {
+        await paciente.deleteOne();
+        res.json({msj: "Paciente Eliminado"});
+    } catch (error){
+        console.log(error);
+    }
+
+};
 
 export {
     agregarPaciente,
--- routes/pacienteRoutes.js
+++ routes/pacienteRoutes.js
@@ -9,9 +9,9 @@ import checkAuth from '../middleware/authMiddleware.js';
 router.post('/', checkAuth, agregarPaciente);
 router.get('/', checkAuth, obtenerPacientes);
 
-router. get(':id', checkAuth, obtenerPaciente);
-router. put(':id', checkAuth, actualizarPaciente);
-router. delete(':id', checkAuth, eliminarPaciente);
+router. get('/:id', checkAuth, obtenerPaciente);
+router. put('/:id', checkAuth, actualizarPaciente);
+router. delete('/:id', checkAuth, eliminarPaciente);
 
 
 
