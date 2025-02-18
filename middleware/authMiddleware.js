62d2c0f9d720c80bb03fdcf2c095648742e300aa
Cristian <cgalarzalppa@gmail.com>
2025-02-17 22:06:56 -0300

    se penso que tenia un error en este middleware, pero en realidad era la declaracion del header en el front

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -27,6 +27,7 @@ const checkAuth = async (req, res, next) => {
         const error = new Error("Token no valido o inexistente");
         res.status(403).json({msg: error.message});
     }
+    
     next();
 };
 
